89a799f9a7e2b7dcb350331cc36efc4c36dbd18d
Josh <josh.duncanson@gmail.com>
2026-06-03 20:44:58 -0400

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat, Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { Analytics } from '@vercel/analytics/next'
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -91,6 +92,7 @@ export default function RootLayout({
         <Navbar />
         <main className="flex-1 pt-24">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
